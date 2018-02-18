916c2cbd27cdd618924e351971cd4a03684cc89b
andpago <andpago@gmail.com>
2018-02-18 16:27:50 +0300

    like

--- like/models.py
+++ like/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class AuthoredMixin(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Like(AuthoredMixin):
+    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object = GenericForeignKey(ct_field='content_type', fk_field='object_id')
