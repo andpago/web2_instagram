4aea33d42a7c83311115c712d58c2bacf5366feb
andpago <andpago@gmail.com>
2018-04-02 13:35:07 +0300

    unsaved changes

--- post/views.py
+++ post/views.py
@@ -1,9 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
 
 from post.models import Post
 from post.serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    author = serializers.ReadOnlyField(source='author.id')
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
